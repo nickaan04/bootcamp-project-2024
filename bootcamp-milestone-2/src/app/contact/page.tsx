f1c5c306326f256bcf34110709704a49909d8492
nickaan04 <nickan04@gmail.com>
2024-11-25 01:12:59 -0800

    milestone 2 complete

--- bootcamp-milestone-2/src/app/contact/page.tsx
+++ bootcamp-milestone-2/src/app/contact/page.tsx
@@ -1,3 +1,12 @@
+import ContactMe from "@/components/Contact/contact";
+
 export default function Contact() {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <h1 className="page-title">Contact Me</h1>
+        <ContactMe />
+      </main>
+    </div>
+  );
 }
--- bootcamp-milestone-2/src/components/Contact/contact.tsx
+++ bootcamp-milestone-2/src/components/Contact/contact.tsx
@@ -1,2 +1,44 @@
 import React from "react";
 import style from "./contact.module.css";
+
+export default function Contact() {
+  return (
+    <form id="contact-form" className={style.contactForm}>
+      <label htmlFor="name" className={style.label}>
+        Name
+      </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        placeholder="Name"
+        required
+        className={style.input}
+      />
+
+      <label htmlFor="email" className={style.label}>
+        Email
+      </label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Email"
+        required
+        className={style.input}
+      />
+
+      <label htmlFor="message" className={style.label}>
+        Message
+      </label>
+      <textarea
+        id="message"
+        name="message"
+        placeholder="Message"
+        required
+        className={style.textarea} />
+
+      <input type="submit" value="Submit" className={style.submitButton} />
+    </form>
+  );
+}
